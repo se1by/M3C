6991985feb3cdbb057cfa510e6842f9a3f43031a
Jonas Seibert <jonas@seibert.ninja>
2016-02-20 23:00:27 +0100

    Fix broken/hacky Slot implementation

--- src/main/java/com/bitbyterstudios/m3c/util/Slot.java
+++ src/main/java/com/bitbyterstudios/m3c/util/Slot.java
@@ -1,18 +1,20 @@
 package com.bitbyterstudios.m3c.util;
 
+import com.bitbyterstudios.m3c.nbt.AbstractTag;
+
 import java.nio.ByteBuffer;
 
 public class Slot {
     private short itemId;
     private byte count;
     private short damage;
-    private byte[] raw_nbt;
+    private AbstractTag nbtTag;
 
-    public Slot(short itemId, byte count, short damage, byte[] raw_nbt) {
+    public Slot(short itemId, byte count, short damage, AbstractTag nbtTag) {
         this.itemId = itemId;
         this.count = count;
         this.damage = damage;
-        this.raw_nbt = raw_nbt;
+        this.nbtTag = nbtTag;
     }
 
     public short getItemId() {
@@ -39,31 +41,21 @@ public class Slot {
         this.damage = damage;
     }
 
-    public byte[] getRaw_nbt() {
-        return raw_nbt;
+    public AbstractTag getNbtTag() {
+        return nbtTag;
     }
 
-    public void setRaw_nbt(byte[] raw_nbt) {
-        this.raw_nbt = raw_nbt;
+    public void setNbtTag(AbstractTag nbtTag) {
+        this.nbtTag = nbtTag;
     }
 
     public static Slot fromByteBuffer(ByteBuffer buffer) {
-        if (!buffer.hasRemaining()) return null; // TODO: Hacky fix, FIX IT
         short itemId = buffer.getShort();
         if (itemId == -1) { // Empty slot
             return new Slot(itemId, (byte) 0, (short) 0, null);
         }
         byte count = buffer.get();
         short damage = buffer.getShort();
-        byte first_nbt = buffer.get();
-        if (first_nbt == 0) { // No nbt data
-            return new Slot(itemId, count, damage, new byte[]{first_nbt});
-        }
-        byte[] nbt_data = new byte[buffer.remaining() + 1];
-        nbt_data[0] = first_nbt;
-        for (int i = 1; buffer.hasRemaining(); i++) {
-            nbt_data[i] = buffer.get();
-        }
-        return new Slot(itemId, count, damage, nbt_data);
+        return new Slot(itemId, count, damage, AbstractTag.fromByteBuffer(buffer, true));
     }
 }
