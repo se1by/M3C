09d70459f4e07bff531ff0510785afdffdb0bfbb
Jonas Seibert <jonas@seibert.ninja>
2016-02-20 23:51:57 +0100

    Add proper EntityProperties implementation

--- src/main/java/com/bitbyterstudios/m3c/packets/receiving/EntityProperties20.java
+++ src/main/java/com/bitbyterstudios/m3c/packets/receiving/EntityProperties20.java
@@ -1,19 +1,23 @@
 package com.bitbyterstudios.m3c.packets.receiving;
 
 import com.bitbyterstudios.m3c.ServerHandler;
+import com.bitbyterstudios.m3c.util.Utilities;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class EntityProperties20 extends ReceivingPacket {
     private int entityId;
-    private byte[] raw_properties;
+    private EntityProperty[] properties;
 
     @Override
     public void handle(ByteBuffer buff, ServerHandler handler) {
         entityId = readVarInt(buff);
-        raw_properties = new byte[buff.remaining()];
-        for (int i = 0; buff.hasRemaining(); i++) {
-            raw_properties[i] = buff.get();
+        properties = new EntityProperty[buff.getInt()];
+        for (int i = 0; i < properties.length; i++) {
+            properties[i] = EntityProperty.fromByteBuffer(buff);
         }
     }
 
@@ -25,11 +29,100 @@ public class EntityProperties20 extends ReceivingPacket {
         this.entityId = entityId;
     }
 
-    public byte[] getRaw_properties() {
-        return raw_properties;
+    public EntityProperty[] getProperties() {
+        return properties;
     }
 
-    public void setRaw_properties(byte[] raw_properties) {
-        this.raw_properties = raw_properties;
+    public void setProperties(EntityProperty[] properties) {
+        this.properties = properties;
+    }
+}
+class EntityProperty {
+    private String key;
+    private double value;
+
+    private List<Modifier> modifiers;
+
+    public EntityProperty(String key, double value, List<Modifier> modifiers) {
+        this.key = key;
+        this.value = value;
+        this.modifiers = modifiers;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public List<Modifier> getModifiers() {
+        return modifiers;
+    }
+
+    public void setModifiers(List<Modifier> modifiers) {
+        this.modifiers = modifiers;
+    }
+
+    public static EntityProperty fromByteBuffer(ByteBuffer buffer) {
+        String key = Utilities.readVarIntPrefixedStringFromByteBuffer(buffer);
+        double value = buffer.getDouble();
+        List<Modifier> modifiers = new ArrayList<>();
+        for (int i = Utilities.readVarInt(buffer); i > 0; i--) {
+            modifiers.add(Modifier.fromByteBuffer(buffer));
+        }
+        return new EntityProperty(key, value, modifiers);
+    }
+}
+
+class Modifier {
+    private UUID uuid;
+    private double amount;
+    private byte operation;
+
+    public Modifier(UUID uuid, double amount, byte operation) {
+        this.uuid = uuid;
+        this.amount = amount;
+        this.operation = operation;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public byte getOperation() {
+        return operation;
+    }
+
+    public void setOperation(byte operation) {
+        this.operation = operation;
+    }
+
+    public static Modifier fromByteBuffer(ByteBuffer buffer) {
+        UUID uuid = new UUID(buffer.getLong(), buffer.getLong());
+        double amount = buffer.getDouble();
+        byte operation = buffer.get();
+        return new Modifier(uuid, amount, operation);
     }
 }
