342b1864eb72f72140013913ef488e88ea8518aa
Jonas Seibert <jonas@seibert.ninja>
2016-08-18 19:50:07 +0200

    Replace anonymous class with lambda

--- HelloWorldPlugin/src/main/java/HelloWorldPlugin.java
+++ HelloWorldPlugin/src/main/java/HelloWorldPlugin.java
@@ -1,18 +1,14 @@
 import com.bitbyterstudios.m3c.Client;
 import com.bitbyterstudios.m3c.ServerHandler;
-import com.bitbyterstudios.m3c.packets.receiving.Chat02;
-import com.bitbyterstudios.m3c.plugin.Listener;
+import com.bitbyterstudios.m3c.packets.v47.receiving.Chat02;
 import com.bitbyterstudios.m3c.plugin.Plugin;
 
 public class HelloWorldPlugin implements Plugin {
 
     public void onEnable(Client client) {
         Client.getLogger().info("Hello World from your plugin!");
-        client.getPluginManager().addListener(Chat02.class, new Listener<Chat02>() {
-            public void handle(Chat02 packet, ServerHandler server) {
-                Client.getLogger().info("Chat message: " + packet.getRawJson());
-            }
-        });
+        client.getPluginManager().addListener(Chat02.class, (Chat02 packet, ServerHandler server)
+                -> Client.getLogger().info("Chat message: " + packet.getRawJson()));
     }
 
     public String getName() {
