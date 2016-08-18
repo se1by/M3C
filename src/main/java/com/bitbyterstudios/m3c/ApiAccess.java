74c72e838808c0d0eb745a29676e7bd19cb132dc
Jonas Seibert <jonas@seibert.ninja>
2016-08-18 20:36:08 +0200

    Refactor ApiAccess

--- src/main/java/com/bitbyterstudios/m3c/ApiAccess.java
+++ src/main/java/com/bitbyterstudios/m3c/ApiAccess.java
@@ -1,6 +1,5 @@
 package com.bitbyterstudios.m3c;
 
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -14,7 +13,6 @@ import org.json.simple.parser.ParseException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 
 public class ApiAccess {
     private static final String AUTH_URI = "https://authserver.mojang.com";
@@ -34,131 +32,71 @@ public class ApiAccess {
 
 
     public static ClientData authenticate(String user, String pass) {
-        CloseableHttpClient client = HttpClients.createDefault();
-        CloseableHttpResponse response = null;
-        HttpPost httpPost = new HttpPost(AUTH_URI + AUTH_END);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("agent", AGENT_OBJECT);
         jsonObject.put("username", user);
         jsonObject.put("password", pass);
-        try {
-            StringEntity stringEntity = new StringEntity(jsonObject.toJSONString(), ContentType.APPLICATION_JSON);
-            httpPost.setEntity(stringEntity);
-            httpPost.setHeader("Content-Type", "application/json");
-            response = client.execute(httpPost);
-            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            String s = br.readLine();
-            JSONObject parsedObject = (JSONObject) parser.parse(s);
-            if (parsedObject.containsKey("error")) {
-                return null;
-            }
-            ClientData data = new ClientData();
-            data.setAccessToken(parsedObject.get("accessToken").toString());
-            data.setClientToken(parsedObject.get("clientToken").toString());
-            data.setUser(((JSONObject) parsedObject.get("selectedProfile")).get("name").toString());
-            data.setUuid(((JSONObject) parsedObject.get("selectedProfile")).get("id").toString());
-            return data;
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                client.close();
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+        JSONObject parsedResult = sendJson(jsonObject, AUTH_URI, AUTH_END);
+        if (parsedResult == null) {
+            return null;
         }
-        return null;
+
+        ClientData data = new ClientData();
+        data.setAccessToken(parsedResult.get("accessToken").toString());
+        data.setClientToken(parsedResult.get("clientToken").toString());
+        data.setUser(((JSONObject) parsedResult.get("selectedProfile")).get("name").toString());
+        data.setUuid(((JSONObject) parsedResult.get("selectedProfile")).get("id").toString());
+        return data;
     }
 
     public static void invalidate(ClientData data) {
-        CloseableHttpClient client = HttpClients.createDefault();
-        CloseableHttpResponse response = null;
-        HttpPost httpPost = new HttpPost(AUTH_URI + INVALIDATE_END);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("clientToken", data.getClientToken());
         jsonObject.put("accessToken", data.getAccessToken());
-        try {
-            StringEntity stringEntity = new StringEntity(jsonObject.toJSONString(), ContentType.APPLICATION_JSON);
-            httpPost.setEntity(stringEntity);
-            httpPost.setHeader("Content-Type", "application/json");
-            response = client.execute(httpPost);
-            if (response.getEntity() == null) { //Empty response = went well
-                return;
-            }
-            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            String s = br.readLine();
-            JSONObject parsedObject = (JSONObject) parser.parse(s);
-            if (parsedObject.containsKey("error")) {
-                Client.getLogger().warning(parsedObject.toJSONString());
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                client.close();
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        sendJson(jsonObject, AUTH_URI, INVALIDATE_END);
     }
 
     public static void sendSessionRequest(String accessToken, String serverId, String uuid) {
-        CloseableHttpClient client = HttpClients.createDefault();
-        CloseableHttpResponse response = null;
-        HttpPost httpPost = new HttpPost(SESSION_URI + JOIN_END);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("accessToken", accessToken);
         jsonObject.put("serverId", serverId);
         jsonObject.put("selectedProfile", uuid);
+        sendJson(jsonObject, SESSION_URI, JOIN_END);
+    }
+
+    private static JSONObject sendJson(JSONObject jsonObject, String uri, String endpoint) {
+        CloseableHttpClient client = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        HttpPost httpPost = new HttpPost(uri + endpoint);
+
         try {
             StringEntity stringEntity = new StringEntity(jsonObject.toJSONString(), ContentType.APPLICATION_JSON);
             httpPost.setEntity(stringEntity);
             httpPost.setHeader("Content-Type", "application/json");
             response = client.execute(httpPost);
             if (response.getEntity() == null) { //Empty response = went well
-                return;
+                return jsonObject;
             }
             BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            String s = br.readLine();
-            JSONObject parsedObject = (JSONObject) parser.parse(s);
+            String rawJson = br.readLine();
+            JSONObject parsedObject = (JSONObject) parser.parse(rawJson);
             if (parsedObject.containsKey("error")) {
                 Client.getLogger().warning(parsedObject.toJSONString());
             }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
+            return parsedObject;
+        } catch (IOException | ParseException exception) {
+            exception.printStackTrace();
         } finally {
             try {
                 client.close();
                 if (response != null) {
                     response.close();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
             }
         }
+        return null;
     }
 }
